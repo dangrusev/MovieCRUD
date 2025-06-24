6e10c148c71fd88cc258fb25a49dc547e2e57ba6
Daniel Rusev <danielgrusev@lewisu.edu>
2025-06-23 21:52:28 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = express();
 require('dotenv').config();
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, '..', 'client')));
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));});
+//FrontEnd deployed seperatly
+// app.use(express.static(path.join(__dirname, '..', 'client')));
+// app.get("/", (req, res) => {res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));});
 
 //Mongo
 app.use(express.urlencoded({extended:true}));
